e2cad62b6c8f42fe9f21f633fb104415930a54f7
Steven Falcones León <140020193+fsteven96@users.noreply.github.com>
2025-06-24 22:37:22 -0500

    first commit

--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -3,9 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Laravel\Sanctum\HasApiTokens;
 class Usuario extends Model
 {
+    use HasFactory, Notifiable, HasApiTokens;
     protected $table = 'Usuarios';         
     protected $primaryKey = 'Id';         
     public $timestamps = false;          
